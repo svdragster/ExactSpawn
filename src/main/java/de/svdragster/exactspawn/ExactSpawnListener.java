d7542e9c30c37de4c1c0f7301e66f7e4b9f93497
Gordon Freeman <svdragster@gmail.com>
2015-01-10 13:10:06 +0100

    Update ExactSpawnListener.java

--- src/main/java/de/svdragster/exactspawn/ExactSpawnListener.java
+++ src/main/java/de/svdragster/exactspawn/ExactSpawnListener.java
@@ -1,8 +1,15 @@
 package de.svdragster.exactspawn;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import net.canarymod.Canary;
 import net.canarymod.api.entity.living.humanoid.Player;
+import net.canarymod.api.world.position.Location;
 import net.canarymod.api.world.position.Vector3D;
+import net.canarymod.chat.ChatFormat;
 import net.canarymod.hook.HookHandler;
+import net.canarymod.hook.command.PlayerCommandHook;
 import net.canarymod.hook.player.ConnectionHook;
 import net.canarymod.hook.player.PlayerRespawnedHook;
 import net.canarymod.hook.player.PlayerRespawningHook;
@@ -10,24 +17,38 @@ import net.canarymod.plugin.PluginListener;
 
 public class ExactSpawnListener implements PluginListener {
 
+	List<Player> dead = new ArrayList<Player>();
+	
+	public boolean isBedSpawn(Player player) {
+		Location spawnLocation = player.getSpawnPosition();
+		spawnLocation.setY(0);
+		Location worldSpawnLocation = player.getWorld().getSpawnLocation();
+		worldSpawnLocation.setY(0);
+		Vector3D v = new Vector3D(spawnLocation);
+		if (v.getDistance(worldSpawnLocation) > 16) {
+			return true;
+		}
+		return false;
+	}
+	
 	@HookHandler
 	public void onPlayerRespawning(PlayerRespawningHook hook) {
 		Player player = hook.getPlayer();
-		if (!hook.isBedSpawn()) {
-			hook.setRespawnLocation(player.getWorld().getSpawnLocation());
-		}
+		dead.add(player);
 	}
 	
 	@HookHandler
 	public void onPlayerRespawned(PlayerRespawnedHook hook) {
 		Player player = hook.getPlayer();
-		Vector3D current = new Vector3D(hook.getLocation());
-		Vector3D worldSpawn = new Vector3D(player.getWorld().getSpawnLocation());
-		current.setY(0);
-		worldSpawn.setY(0);
-		double distance = current.getDistance(worldSpawn);
-		if (distance <= 18) {
-			player.teleportTo(player.getWorld().getSpawnLocation());
+		if (dead.contains(player)) {
+			dead.remove(player);
+			if (isBedSpawn(player)) {
+				player.teleportTo(player.getSpawnPosition());
+			} else if (player.hasHome()) {
+				player.teleportTo(player.getHome());
+			} else {
+				player.teleportTo(player.getWorld().getSpawnLocation());
+			}
 		}
 	}
 	
@@ -37,5 +58,42 @@ public class ExactSpawnListener implements PluginListener {
 			Player player = hook.getPlayer();
 			player.teleportTo(player.getWorld().getSpawnLocation());
 		}
-	}	
+	}
+	
+	@HookHandler
+	public void onCommand(PlayerCommandHook hook) {
+		Player player = hook.getPlayer();
+		String[] message = hook.getCommand();
+		String p0 = message[0];
+		if (p0.equalsIgnoreCase("/spawn")) {
+			if (message.length >= 2) {
+				String strWorld = message[1];
+				if (message.length >= 3) {
+					String strOtherPlayer = message[2];
+					Player otherPlayer = Canary.getServer().matchPlayer(strOtherPlayer);
+					if (otherPlayer != null) {
+						if (strWorld.equalsIgnoreCase(otherPlayer.getWorld().getName())) {
+							otherPlayer.teleportTo(player.getWorld().getSpawnLocation());
+							otherPlayer.message(ChatFormat.YELLOW + "You are now at the spawn");
+							player.message(ChatFormat.YELLOW + otherPlayer.getName() + " is now at the spawn");
+							hook.setCanceled();
+						}
+					} else {
+						player.notice(strOtherPlayer + "is not online.");
+						hook.setCanceled();
+					}
+				} else {
+					if (strWorld.equalsIgnoreCase(player.getWorld().getName())) {
+						player.teleportTo(player.getWorld().getSpawnLocation());
+						player.message(ChatFormat.YELLOW + "You are now at the spawn");
+						hook.setCanceled();
+					}
+				}
+			} else {
+				player.teleportTo(player.getWorld().getSpawnLocation());
+				player.message(ChatFormat.YELLOW + "You are now at the spawn");
+				hook.setCanceled();
+			}
+		}
+	}
 }
